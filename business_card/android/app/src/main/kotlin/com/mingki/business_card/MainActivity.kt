f31b4c8102e472196c56c8fea7ece333274ad13d
qkralswl689@gmail.com <qkralswl689@gmail.com>
2023-11-14 20:11:46 +0900

    make business_card project

---
+++ business_card/android/app/src/main/kotlin/com/mingki/business_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mingki.business_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ business_card/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:business_card/main_screen.dart';
+import 'package:business_card/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Business Card',
+      debugShowCheckedModeBanner: false, // 디버그 라벨 제거
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/main': (context) => const MainScreen(),
+      },
+    );
+  }
+}
