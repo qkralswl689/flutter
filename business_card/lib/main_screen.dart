08f2c0c669977db4f8ffebb88841ddf4a0db2bf2
qkralswl689@gmail.com <qkralswl689@gmail.com>
2023-11-16 20:15:46 +0900

    make business card

--- business_card/lib/main_screen.dart
+++ business_card/lib/main_screen.dart
@@ -8,8 +8,193 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  TextEditingController introduceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.accessibility_new,
+          color: Colors.black,
+          size: 32,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          '발전하는 개발자 박민지를 소개 합니다',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(16),
+              width: double.infinity,
+              height: 150,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/dog.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            //이름
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Text(
+                      '이름',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'MingKi',
+                  ),
+                ],
+              ),
+            ),
+            //나이
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Text(
+                      '나이',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '33',
+                  ),
+                ],
+              ),
+            ),
+            // 취미
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Text(
+                      '취미',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '주짓수',
+                  ),
+                ],
+              ),
+            ),
+            // 직업
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Text(
+                      '직업',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '개발자',
+                  ),
+                ],
+              ),
+            ),
+            // 학력
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Text(
+                      '학력',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '명지대학교',
+                  ),
+                ],
+              ),
+            ),
+            // MBTI
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Text(
+                      'MBTI',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'ESFJ',
+                  ),
+                ],
+              ),
+            ),
+            // 자기소개
+            Container(
+              margin: const EdgeInsets.only(left: 16, top: 16),
+              child: const Text(
+                '자기소개',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              child: TextField(
+                maxLines: 5,
+                controller: introduceController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Color(0xffd9d9d9),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
